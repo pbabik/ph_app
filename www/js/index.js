bf85cba51bef01fa1e8ca372bdd40e4d80275684
Michal Mackiewicz <michalmacki@gmail.com>
2016-02-22 19:32:24 +0100

    hello word, ....

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,30 @@
 if(typeof L != 'undefined') {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var map = L.map('map').setView([50, 20], 13);
 
     L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
+
+    $.getJSON('http://dev.apps.divi.pl/api/features/ODY.pR0dh8HyYNdcQYHWA_dTPuT3G-4',{},function(data){
+        var geojsonMarkerOptions = {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+
+        L.geoJson(data.features, {
+            pointToLayer: function (feature, latlng) {
+                var mrkr = L.circleMarker(latlng, geojsonMarkerOptions);
+                mrkr.bindPopup(feature.properties.name);
+                return mrkr;
+            }
+        }).addTo(map);
+    });
 }
 
 else {
     document.getElementById('div2').innerHTML = 'No Leaflet in the village';
 }
-
-var kachel = document.createElement('img');
-kachel.setAttribute('src','http://a.tile.openstreetmap.org/13/4489/2805.png');
-document.body.appendChild(kachel);
