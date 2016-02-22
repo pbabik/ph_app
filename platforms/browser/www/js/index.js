84732af41980be288a7702f8906b5c15cb86d221
Michal Mackiewicz <michalmacki@gmail.com>
2016-02-22 18:49:22 +0100

    map - another try

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,11 +1,13 @@
-if(typeof jQuery != 'undefined') {
-    var sub = $('<h1>');
-    sub.html('Hello world');
-    $('#div2').append(sub);
+if(typeof L != 'undefined') {
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
 }
 
 else {
-    document.getElementById('div2').innerHTML = 'No jQuery in the village';
+    document.getElementById('div2').innerHTML = 'No Leaflet in the village';
 }
 
 var kachel = document.createElement('img');
